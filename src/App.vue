5ee4bd6d74089e4bf54115a53d75d7f71b44a809
Wusc95 <1223348519@qq.com>
2019-12-13 11:30:11 +0800

    项目起步-element-ui整合

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
+    <el-button type="success">成功按钮</el-button>
     <router-view></router-view>
   </div>
 </template>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+//引入element-ui
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
 //引入vue-router
 import router from './router/router.js'
+//注册
+Vue.use(ElementUI);
 
 Vue.config.productionTip = false
 
