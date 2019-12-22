85620e735aa916012e672249fb7c27083ed7dd15
Wusc95 <1223348519@qq.com>
2019-12-22 17:50:06 +0800

    程序优化-抽取邮箱 手机号验证函数

--- src/views/index/user/components/editDialog.vue
+++ src/views/index/user/components/editDialog.vue
@@ -42,35 +42,37 @@
 </template>
 
 <script>
+//导入表单验证规则
+import {checkPhone,checkEmail}  from '../../../../utils/validator.js'
 import { userEdit } from "../../../../api/user.js";
 export default {
   data() {
-    //手机号验证规则
-    var checkPhone = (rule, value, callback) => {
-      if (!value) {
-        return callback(new Error("手机号不能为空"));
-      } else {
-        const reg = /^(0|86|17951)?(13[0-9]|15[012356789]|166|17[3678]|18[0-9]|14[57])[0-9]{8}$/;
-        if (reg.test(value) == true) {
-          callback();
-        } else {
-          return callback(new Error("请输入正确手机号"));
-        }
-      }
-    };
-    //邮箱验证规则
-    var checkEmail = (rule, value, callback) => {
-      if (!value) {
-        return callback(new Error("邮箱不能为空"));
-      } else {
-        const regEmail = /\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*/;
-        if (regEmail.test(value) == true) {
-          callback();
-        } else {
-          return callback(new Error("请输入正确的邮箱"));
-        }
-      }
-    };
+    // //手机号验证规则
+    // var checkPhone = (rule, value, callback) => {
+    //   if (!value) {
+    //     return callback(new Error("手机号不能为空"));
+    //   } else {
+    //     const reg = /^(0|86|17951)?(13[0-9]|15[012356789]|166|17[3678]|18[0-9]|14[57])[0-9]{8}$/;
+    //     if (reg.test(value) == true) {
+    //       callback();
+    //     } else {
+    //       return callback(new Error("请输入正确手机号"));
+    //     }
+    //   }
+    // };
+    // //邮箱验证规则
+    // var checkEmail = (rule, value, callback) => {
+    //   if (!value) {
+    //     return callback(new Error("邮箱不能为空"));
+    //   } else {
+    //     const regEmail = /\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*/;
+    //     if (regEmail.test(value) == true) {
+    //       callback();
+    //     } else {
+    //       return callback(new Error("请输入正确的邮箱"));
+    //     }
+    //   }
+    // };
     return {
       //新增对话框表单
       editForm: {
--- src/views/login/login.vue
+++ src/views/login/login.vue
@@ -118,37 +118,40 @@
 
 <script>
 // import axios from "axios";
+//导入表单验证规则
+import {checkPhone,checkEmail}  from '../../utils/validator.js'
+
 import { login, register, getsmsCode } from "../../api/login.js";
 import { setToken } from "../../utils/token.js";
 export default {
   data() {
     //登陆验证逻辑
-    var checkPhone = (rule, value, callback) => {
-      if (!value) {
-        return callback(new Error("手机号不能为空"));
-      } else {
-        const reg = /^(0|86|17951)?(13[0-9]|15[012356789]|166|17[3678]|18[0-9]|14[57])[0-9]{8}$/;
-        if (reg.test(value) == true) {
-          callback();
-        } else {
-          return callback(new Error("请输入正确手机号"));
-        }
-      }
-    };
+    // var checkPhone = (rule, value, callback) => {
+    //   if (!value) {
+    //     return callback(new Error("手机号不能为空"));
+    //   } else {
+    //     const reg = /^(0|86|17951)?(13[0-9]|15[012356789]|166|17[3678]|18[0-9]|14[57])[0-9]{8}$/;
+    //     if (reg.test(value) == true) {
+    //       callback();
+    //     } else {
+    //       return callback(new Error("请输入正确手机号"));
+    //     }
+    //   }
+    // };
     //注册验证逻辑
     //验证邮箱
-    var checkEmail = (rule, value, callback) => {
-      if (!value) {
-        return callback(new Error("邮箱不能为空"));
-      } else {
-        const regEmail = /\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*/;
-        if (regEmail.test(value) == true) {
-          callback();
-        } else {
-          return callback(new Error("请输入正确的邮箱"));
-        }
-      }
-    };
+    // var checkEmail = (rule, value, callback) => {
+    //   if (!value) {
+    //     return callback(new Error("邮箱不能为空"));
+    //   } else {
+    //     const regEmail = /\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*/;
+    //     if (regEmail.test(value) == true) {
+    //       callback();
+    //     } else {
+    //       return callback(new Error("请输入正确的邮箱"));
+    //     }
+    //   }
+    // };
 
     return {
       form: {
