193ec1e98003d163be675990799dbf1d03fec0cd
Wusc95 <1223348519@qq.com>
2019-12-22 21:40:40 +0800

    权限模块 - 路由显示权限

--- src/views/index/index.vue
+++ src/views/index/index.vue
@@ -17,23 +17,23 @@
       <!-- 左侧 -->
       <el-aside class="aside" width="auto">
         <el-menu :default-active="$route.path" class="el-menu-vertical-demo list-show" router :collapse="isCollapse">
-          <el-menu-item index="/index/chart">
+          <el-menu-item index="/index/chart" v-if="['管理员','老师'].includes($store.state.userInfo.role)">
             <i class="el-icon-pie-chart"></i>
             <span slot="title">数据概览</span>
           </el-menu-item>
-          <el-menu-item index="/index/user">
+          <el-menu-item index="/index/user" v-if="['管理员'].includes($store.state.userInfo.role)">
             <i class="el-icon-user"></i>
             <span slot="title">用户列表</span>
           </el-menu-item>
-          <el-menu-item index="/index/question">
+          <el-menu-item index="/index/question" v-if="['管理员','老师','学生'].includes($store.state.userInfo.role)">
             <i class="el-icon-edit-outline"></i>
             <span slot="title">题库列表</span>
           </el-menu-item>
-          <el-menu-item index="/index/enterprise">
+          <el-menu-item index="/index/enterprise" v-if="['管理员','老师'].includes($store.state.userInfo.role)">
             <i class="el-icon-office-building"></i>
             <span slot="title">企业列表</span>
           </el-menu-item>
-          <el-menu-item index="/index/subject">
+          <el-menu-item index="/index/subject" v-if="['管理员','老师'].includes($store.state.userInfo.role)">
             <i class="el-icon-notebook-2"></i>
             <span slot="title">学科列表</span>
           </el-menu-item>
